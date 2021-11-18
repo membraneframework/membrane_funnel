6ea47de0d6e4056cdf61f0f68227a935277fb536
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-18 10:57:06 +0100

    bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Funnel.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_funnel_plugin"
 
   def project do
@@ -36,7 +36,7 @@ defmodule Membrane.Funnel.Plugin.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
+      {:membrane_core, "~> 0.8.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: :dev, runtime: false}
