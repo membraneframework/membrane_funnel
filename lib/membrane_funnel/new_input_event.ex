c6290623955bbc23b1ea832664fa7240b8be1cce
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2024-12-18 17:00:57 +0100

    Adjust to incorporating Funnel into membrane_core

--- lib/membrane_funnel/new_input_event.ex
+++ lib/membrane_funnel/new_input_event.ex
@@ -1,9 +1,11 @@
-defmodule Membrane.Funnel.NewInputEvent do
-  @moduledoc """
-  Event sent each time new element is linked (via funnel input pad) after playing pipeline.
-  """
-  @derive Membrane.EventProtocol
+if :code.module_status(Membrane.Funnel.NewInputEvent) == :not_loaded do
+  defmodule Membrane.Funnel.NewInputEvent do
+    @moduledoc """
+    Event sent each time new element is linked (via funnel input pad) after playing pipeline.
+    """
+    @derive Membrane.EventProtocol
 
-  @type t :: %__MODULE__{}
-  defstruct []
+    @type t :: %__MODULE__{}
+    defstruct []
+  end
 end
