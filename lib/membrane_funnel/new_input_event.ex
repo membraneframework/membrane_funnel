5fad92d337923f76b7e3a83e00418afa153a7b70
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2024-12-23 16:43:37 +0100

    Adjust to incorporating Funnel into membrane_core (#17)
    * Adjust to incorporating Funnel into membrane_core
    
    * Implement review comments

--- lib/membrane_funnel/new_input_event.ex
+++ lib/membrane_funnel/new_input_event.ex
@@ -1,9 +1,13 @@
-defmodule Membrane.Funnel.NewInputEvent do
-  @moduledoc """
-  Event sent each time new element is linked (via funnel input pad) after playing pipeline.
-  """
-  @derive Membrane.EventProtocol
+module_name = Membrane.Funnel.NewInputEvent
 
-  @type t :: %__MODULE__{}
-  defstruct []
+if not Code.ensure_loaded?(module_name) do
+  defmodule module_name do
+    @moduledoc """
+    Event sent each time new element is linked (via funnel input pad) after playing pipeline.
+    """
+    @derive Membrane.EventProtocol
+
+    @type t :: %__MODULE__{}
+    defstruct []
+  end
 end
